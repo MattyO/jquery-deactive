7b5007b98063f54306954d14a37c7864c75426e5
Matt ODonnell <matt.odonnell@leandog.com>
2013-11-23 20:48:50 -0500

    everything in its place

--- js/deactivate.js
+++ js/deactivate.js
@@ -1,4 +1,5 @@
 $.fn.deactivate = function(){
+    $('head').append('<link href="css/deactivate.css" rel="stylesheet" type="text/stylesheet" />');
     return this.each(function(){
         var new_div = $(document.createElement('div'))
             .height($(this).height())
